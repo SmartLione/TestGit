f43fd489abc4c08413e353fe6d1ba0db6e5a5213
Alex <etechnician@gmail.com>
2021-01-20 16:10:37 +0300

    Add testGit

---
+++ Assets/Scripts/TestGitHub.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestGitHub : MonoBehaviour
+{
+    public GameObject go1;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        go1 = GameObject.Find("Cube");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
